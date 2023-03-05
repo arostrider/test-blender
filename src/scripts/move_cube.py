c96186c2a232d4d53fe09944fcb336a520eb6661
Marko <mlaketic@outlook.com>
2023-03-05 17:10:40 +0100

    cosmetic refactor move_cube.py

--- src/scripts/move_cube.py
+++ src/scripts/move_cube.py
@@ -1,8 +1,10 @@
 import bpy
 
-bpy.context.preferences.view.show_splash = False
+if __name__ == "__main__":
+    bpy.context.preferences.view.show_splash = False
 
-bpy.context.active_object.location = (1, 2, 3)
+    bpy.context.active_object.location = (0, 0, 0)
 
-assert tuple(bpy.context.active_object.location) == (0, 0, 0), f"Actual: {tuple(bpy.context.active_object.location)} "\
-                                                               f"Expected: (0, 0, 0)"
+    assert tuple(bpy.context.active_object.location) == (0, 0, 0), \
+        f"Actual: {tuple(bpy.context.active_object.location)} " \
+        f"Expected: (0, 0, 0)"
