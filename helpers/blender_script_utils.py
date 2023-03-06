5d8e0ee8136d54019a0ced4d64a91088d7f55ccd
Marko <mlaketic@outlook.com>
2023-03-07 00:18:49 +0100

    add new_shader and new_material to blender_script_utils.py

--- helpers/blender_script_utils.py
+++ helpers/blender_script_utils.py
@@ -99,3 +99,44 @@ def add_cone(
         rotation=rotation,
         scale=scale
     )
+
+
+def new_material(material_id):
+    mat = bpy.data.materials.get(material_id)
+
+    if mat is None:
+        mat = bpy.data.materials.new(name=material_id)
+
+    mat.use_nodes = True
+
+    if mat.node_tree:
+        mat.node_tree.links.clear()
+        mat.node_tree.nodes.clear()
+
+    return mat
+
+
+def new_shader(material_id, type, r, g, b):
+    mat = new_material(material_id)
+
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+    output = nodes.new(type='ShaderNodeOutputMaterial')
+
+    if type == "diffuse":
+        shader = nodes.new(type='ShaderNodeBsdfDiffuse')
+        nodes["Diffuse BSDF"].inputs[0].default_value = (r, g, b, 1)
+
+    elif type == "emission":
+        shader = nodes.new(type='ShaderNodeEmission')
+        nodes["Emission"].inputs[0].default_value = (r, g, b, 1)
+        nodes["Emission"].inputs[1].default_value = 1
+
+    elif type == "glossy":
+        shader = nodes.new(type='ShaderNodeBsdfGlossy')
+        nodes["Glossy BSDF"].inputs[0].default_value = (r, g, b, 1)
+        nodes["Glossy BSDF"].inputs[1].default_value = 0
+
+    links.new(shader.outputs[0], output.inputs[0])
+
+    return mat
