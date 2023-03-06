6984c34ff8c8a72813e9763a234d772c2706efeb
Marko <mlaketic@outlook.com>
2023-03-06 23:53:37 +0100

    refactor move_cube.py to match create_arbitrary_shapes.py

--- scripts/move_cube.py
+++ scripts/move_cube.py
@@ -2,21 +2,20 @@ import bpy
 import helpers.blender_script_utils as bsu
 from helpers.math_wrap import roundf
 
-
-def move_cube(location: tuple[float]):
-    bpy.context.preferences.view.show_splash = False
-    bpy.context.active_object.location = location
-
-
 if __name__ == "__main__":
     args = bsu.blender_script_args()
     print(args)
 
+    # init
     bsu.set_blender_render_settings(x_res=int(args['x']), y_res=int(args['y']), file_format='JPEG')
+    bpy.context.preferences.view.show_splash = False
 
+    # move default cube to destination provided in cmdline args
     destination = tuple(float(coord) for coord in args['free_vals'])
-    move_cube(destination)
+    bpy.context.active_object.location = destination
 
+    # round coordinates of active object (should be default cube)
+    # to avoid assertion error due to high precision difference
     new_location = tuple(roundf(coord) for coord in bpy.context.active_object.location)
 
     assert new_location == destination, f"Actual: {new_location} Expected: {destination}"
